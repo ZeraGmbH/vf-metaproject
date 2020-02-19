3871aca079841e0c4f4950c914e77fcfd8cad509
bhamacher <b.hamacher@zera.de>
2020-02-19 07:13:27 +0100

    New Qt compatible Superbuild concept
    CMakeLists.txt: List with all subprojects
    ZeraSuperBuild.cmake: provides zera superbuild functions
    .gitmodules: List with gitmodules used in superbuild
    README.md: instructions how to use superbuild added
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- README.md
+++ README.md
@@ -11,3 +11,149 @@ QLoggingCategory::setFilterRules(loggingFilters.join("\n"));
 
 ```
 
+# Superbuild Information
+
+This Superbuild combines libs, plugins and services in one metaproject
+
+**SubProjects**
+
+* vf-helpers
+* vf-tcp
+* xiqnet
+* vf-crypto-bridge
+* vf-event
+* vf-storage-hash
+* vf-net2
+* vf-protobuf
+* vf-qml
+* vf-script
+* vf-logger
+* vf-database-recorder
+* vf-database-replay
+* vf-debugger
+* vf-unittest2
+* vf-declarative-gui
+* zera-setup2
+
+
+
+## clone Project
+
+in your terminal:
+
+```
+git clone <repo> 
+cd /<path>/<to>/<repo>
+git submodule update --init --recursive
+git submodule update --remote
+```
+
+
+## Setup Project
+Following folders must be set:
+
+* CMAKE_PREFIX_PATH=/usr
+* CMAKE_INSTALL_PREFIX=/usr
+* CMAKE_INSTALL_SYSCONFDIR=/etc
+
+CMAKE_PREFIX_PATH has to be equal to /usr;${CMAKE_INSTALL_PREFIX}.
+It is recommendet to set the variables not to the hosts systemroot.
+
+For example use:
+
+CMAKE_PREFIX_PATH=/usr;/${some}/${folder}/usr
+CMAKE_INSTALL_PREFIX=/${some}/${folder}/usr
+CMAKE_INSTALL_SYSCONFDIR=/${some}/${folder}/etc
+
+### in Terminal 
+
+To setup the Project in your terminal go to your desired build location and call:
+
+```
+cmake -DCMAKE_PREFIX_PATH="/usr;${install_dir}" -DCMAKE_INSTALL_PREFIX="${install_dir}"  
+-DCMAKE_INSTALL_SYSCONFDIR="${config_dir}" -S /<path>/<to>/<repo> 
+```
+
+To build the project call 
+
+```
+make -j8
+```
+in your build directory.
+
+
+### in qt Creator
+
+* Go to open project and choose the toplevel CMakeLists.txt
+* Configure project
+* Go to Projects
+* Delete all build steps 
+* add Custom process step 
+  * Command `make`
+  * Arguments `-j8`
+* Add the required cmake variables and set them as described above
+
+Now you are free to choose which project to build by setting the option for the corresponding project.
+Please be sure to set isPartialBuild if you do not build all projects. Note that you have to build all projects 
+at least once.
+
+## Add Subproject to SuperBuild
+
+### Add to git 
+To add a subproject first add it in git with following command:
+
+```
+git submodule add -b <branch> git@github.com:ZeraGmbH/<project> <path>/<project>
+```
+
+for example:
+
+
+```
+git submodule add -b master git@github.com:ZeraGmbH/vf-declarative-gui.git guis/vf-declarative-gui
+```
+
+check if the submodule appears in .gitmodules.
+
+
+### Add to cmake
+
+
+To add the project to cmake open CMakeLists.txt and add following lines:
+
+```
+option(build_<name> "activate or deactivate build" ON)
+...
+set(name <project>)
+set(path <path>)
+set(deps <dep1> <dep2> <dep3>)
+if(${build_${name}})
+    add_sub_project_deps(${name} ${path} "deps")
+endif()
+```
+
+or if the project has no deps
+
+```
+option(build_<name> "activate or deactivate build" ON)
+...
+set(name <project>)
+set(path <path>)
+if(${build_${name}})
+    add_sub_project_deps(${name} ${path})
+endif()
+```
+
+Please note that the project order is important. The project must be added after all projects it depends on. 
+The dependencies will determine the build order during make. 
+
+## issues
+
+* 1 
+If one of these steps is executed wrong it might happen that the build will not work. 
+In this case delete the build folder. In case you are using qt creator remove CMakeLists.tyt.user and import the project again. 
+Then repeat the setup steps.
+
+
+
+
---
+++ ZeraSuperBuild.cmake
@@ -0,0 +1,123 @@
+
+# This extension simplifies cmake superprojects
+# 
+# 1. create a git repo 
+# 2. add submodules to repo
+# 3. create cmake SuperProject file and include this file
+# 4. use Macro as follows
+#  
+# case1: Project without dependencies
+#
+# set(name projectFolder)
+# set(path Path/to/))
+# add_sub_project(${name} ${path})
+#
+# case2: Project with dependencies
+#
+# set(name projectFolder)
+# set(path Path/to/)
+# set(deps other external projects)
+# add_sub_project_deps(${name} ${path} "deps")
+#
+# Please Note that the order is important. Because cmake can not resolve the deps otherwise
+
+
+
+
+include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
+
+option(isPartialBuild "set to ON if not all projects are build" OFF)
+
+#Silent subdirs
+#force all find_package calls to be Quiet
+macro(find_package)	
+    string(REGEX REPLACE "REQUIRED" "QUIET" OUT "${ARGV}")
+    _find_package(${OUT})
+endmacro()
+
+macro(pkg_check_modules)
+
+endmacro()
+
+#macro(PROTOBUF_GENERATE_CPP)
+
+#endmacro()
+
+#macro(execute_process)
+
+#endmacro()
+
+#macro(add_library)
+
+#endmacro()
+
+
+#Suppressing linking libs in subdirs of the superproject
+macro(target_link_libraries)	
+
+endmacro()
+
+macro(add_sub_project name path)
+    #message(STATUS "External Project at: " ${CMAKE_SOURCE_DIR}/${path}/${name})
+    #message(STATUS "Building in: " ${CMAKE_CURRENT_BINARY_DIR}/${path}/${name})
+    #message(STATUS "Project name is: " ${name})
+    #message(STATUS "Install in: " ${CMAKE_INSTALL_PREFIX})
+    #message(STATUS "Locate Libs in: " ${CMAKE_PREFIX_PATH})    
+
+    # We want to see the project in QT creator. Therefore we have to add the subdirectory
+    # But we do not want to use it. EXCLUDE_FROM_ALL suppresses build in this directory
+    add_subdirectory(${CMAKE_SOURCE_DIR}/${path}/${name} EXCLUDE_FROM_ALL)
+    #Lets build the project as external project now   
+
+    ExternalProject_Add(${name}_ext
+            SOURCE_DIR ${CMAKE_SOURCE_DIR}/${path}/${name}
+            BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${path}/${name}
+            CMAKE_CACHE_ARGS 
+                -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
+                -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH}
+                -DCMAKE_INSTALL_SYSCONFDIR:PATH=${CMAKE_INSTALL_SYSCONFDIR}
+
+       )
+endmacro()
+
+
+macro(add_sub_project_deps name path _depends)
+    #message(STATUS "External Project at: " ${CMAKE_SOURCE_DIR}/${path}/${name})
+    #message(STATUS "Building in: " ${CMAKE_CURRENT_BINARY_DIR}/${path}/${name})
+    #message(STATUS "Project name is: " ${name})
+    #message(STATUS "Install in: " ${CMAKE_INSTALL_PREFIX})
+    #message(STATUS "Locate Libs in: " ${CMAKE_PREFIX_PATH})    
+
+    #message(STATUS "Project has deps: " ${${_depends}})
+
+    #The internal names are extended with _ext to prevent conflicts with the target names used in the subdirectories.
+    foreach(TMP_DEP ${${_depends}})
+         list(APPEND deps_ext ${TMP_DEP}_ext)
+    endforeach()
+    if(NOT isPartialBuild)
+        # We want to see the project in QT creator. Therefore we have to add the subdirectory
+        # But we do not want to use it. EXCLUDE_FROM_ALL suppresses build in this directory
+        add_subdirectory(${CMAKE_SOURCE_DIR}/${path}/${name} EXCLUDE_FROM_ALL)
+        #Lets build the project as external project now
+        ExternalProject_Add(${name}_ext
+                SOURCE_DIR ${CMAKE_SOURCE_DIR}/${path}/${name}
+                BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${path}/${name}
+                CMAKE_CACHE_ARGS 
+                    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
+                    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH}
+                    -DCMAKE_INSTALL_SYSCONFDIR:PATH=${CMAKE_INSTALL_SYSCONFDIR}
+                DEPENDS
+                    ${deps_ext}
+       )
+    else()
+         ExternalProject_Add(${name}_ext
+            SOURCE_DIR ${CMAKE_SOURCE_DIR}/${path}/${name}
+            BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${path}/${name}
+            CMAKE_CACHE_ARGS 
+                -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
+                -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH}
+                -DCMAKE_INSTALL_SYSCONFDIR:PATH=${CMAKE_INSTALL_SYSCONFDIR}
+          )
+
+    endif()
+endmacro()
