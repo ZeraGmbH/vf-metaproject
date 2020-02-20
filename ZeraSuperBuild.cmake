0cf2eba3ff55ea830902b853bb3dd6e395929a17
bhamacher <b.hamacher@zera.de>
2020-02-20 13:06:11 +0100

    Improved qt creator compatibility
    CMakeLists.txt: adapted add_subproject call
    ZeraSuperBuild.cmake: adapted add_subproject macro. Distinguish between firstBuild or not.
    README.md: adapted manual to the new steps.
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- ZeraSuperBuild.cmake
+++ ZeraSuperBuild.cmake
@@ -5,14 +5,6 @@
 # 2. add submodules to repo
 # 3. create cmake SuperProject file and include this file
 # 4. use Macro as follows
-#  
-# case1: Project without dependencies
-#
-# set(name projectFolder)
-# set(path Path/to/))
-# add_sub_project(${name} ${path})
-#
-# case2: Project with dependencies
 #
 # set(name projectFolder)
 # set(path Path/to/)
@@ -26,81 +18,43 @@
 
 include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
 
-option(isPartialBuild "set to ON if not all projects are build" OFF)
+option(firstBuild "set to on for the first project build/set off when you start working with the project" ON)
 
+if(firstBuild)
 #Silent subdirs
 #force all find_package calls to be Quiet
-macro(find_package)	
-    string(REGEX REPLACE "REQUIRED" "QUIET" OUT "${ARGV}")
-    _find_package(${OUT})
-endmacro()
+    macro(find_package)
+        string(REGEX REPLACE "REQUIRED" "QUIET" OUT "${ARGV}")
+        _find_package(${OUT})
+    endmacro()
 
-macro(pkg_check_modules)
+    macro(pkg_check_modules)
 
-endmacro()
-
-#macro(PROTOBUF_GENERATE_CPP)
-
-#endmacro()
-
-#macro(execute_process)
-
-#endmacro()
-
-#macro(add_library)
-
-#endmacro()
+    endmacro()
 
 
-#Suppressing linking libs in subdirs of the superproject
-macro(target_link_libraries)	
+    macro(target_link_libraries)
 
-endmacro()
+    endmacro()
 
-macro(add_sub_project name path)
-    #message(STATUS "External Project at: " ${CMAKE_SOURCE_DIR}/${path}/${name})
-    #message(STATUS "Building in: " ${CMAKE_CURRENT_BINARY_DIR}/${path}/${name})
-    #message(STATUS "Project name is: " ${name})
-    #message(STATUS "Install in: " ${CMAKE_INSTALL_PREFIX})
-    #message(STATUS "Locate Libs in: " ${CMAKE_PREFIX_PATH})    
+endif()
 
-    # We want to see the project in QT creator. Therefore we have to add the subdirectory
-    # But we do not want to use it. EXCLUDE_FROM_ALL suppresses build in this directory
-    add_subdirectory(${CMAKE_SOURCE_DIR}/${path}/${name} EXCLUDE_FROM_ALL)
-    #Lets build the project as external project now   
-
-    ExternalProject_Add(${name}_ext
-            SOURCE_DIR ${CMAKE_SOURCE_DIR}/${path}/${name}
-            BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${path}/${name}
-            CMAKE_CACHE_ARGS 
-                -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-                -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH}
-                -DCMAKE_INSTALL_SYSCONFDIR:PATH=${CMAKE_INSTALL_SYSCONFDIR}
-                -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-                -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
-       )
-endmacro()
 
 
 macro(add_sub_project_deps name path _depends)
-    #message(STATUS "External Project at: " ${CMAKE_SOURCE_DIR}/${path}/${name})
-    #message(STATUS "Building in: " ${CMAKE_CURRENT_BINARY_DIR}/${path}/${name})
-    #message(STATUS "Project name is: " ${name})
-    #message(STATUS "Install in: " ${CMAKE_INSTALL_PREFIX})
-    #message(STATUS "Locate Libs in: " ${CMAKE_PREFIX_PATH})    
-
-    #message(STATUS "Project has deps: " ${${_depends}})
-
     #The internal names are extended with _ext to prevent conflicts with the target names used in the subdirectories.
     foreach(TMP_DEP ${${_depends}})
          list(APPEND deps_ext ${TMP_DEP}_ext)
     endforeach()
-    add_subdirectory(${CMAKE_SOURCE_DIR}/${path}/${name} EXCLUDE_FROM_ALL)
-    if(NOT isPartialBuild)
+
+    if(NOT firstBuild)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/${path}/${name})
+    else()
         # We want to see the project in QT creator. Therefore we have to add the subdirectory
         # But we do not want to use it. EXCLUDE_FROM_ALL suppresses build in this directory
 
         #Lets build the project as external project now
+
         ExternalProject_Add(${name}_ext
                 SOURCE_DIR ${CMAKE_SOURCE_DIR}/${path}/${name}
                 BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${path}/${name}
@@ -113,17 +67,5 @@ macro(add_sub_project_deps name path _depends)
                 DEPENDS
                     ${deps_ext}
        )
-    else()
-         ExternalProject_Add(${name}_ext
-            SOURCE_DIR ${CMAKE_SOURCE_DIR}/${path}/${name}
-            BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${path}/${name}
-            CMAKE_CACHE_ARGS 
-                -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-                -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH}
-                -DCMAKE_INSTALL_SYSCONFDIR:PATH=${CMAKE_INSTALL_SYSCONFDIR}
-                -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-                -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
-          )
-
     endif()
 endmacro()
